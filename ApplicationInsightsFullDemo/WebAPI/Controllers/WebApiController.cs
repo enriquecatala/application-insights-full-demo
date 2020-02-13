87348deb0e76bd28d762306efa93a2565e5918a2
Enrique Catala Bañuls <enrique@enriquecatala.com>
2020-02-13 16:22:23 +0100

    solution

---
+++ ApplicationInsightsFullDemo/WebAPI/Controllers/WebApiController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WebApiController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WebApiController> _logger;
+
+        public WebApiController(ILogger<WebApiController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpGet]
+        [Route("api/Subscription/{subscriptionId}")]
+        public async Task<object> IsValid(string subscriptionId)
+        {
+            return ("trololol: "+ subscriptionId);
+        }
+    }
+}
