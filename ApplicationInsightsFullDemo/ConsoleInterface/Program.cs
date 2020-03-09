ce72cce8baa105b093c6c544d37b30ebe24197e9
Enrique Catala Bañuls <enrique@enriquecatala.com>
2020-03-09 09:26:15 +0100

    clean init

--- ApplicationInsightsFullDemo/ConsoleInterface/Program.cs
+++ ApplicationInsightsFullDemo/ConsoleInterface/Program.cs
@@ -35,6 +35,7 @@ namespace ConsoleInterface
             _urlApiSqlServer = configuration.GetSection("UrlApiSqlServer").Value;
             _urlApiCosmos = configuration.GetSection("UrlApiCosmos").Value;
 
+            TestCosmosDBInitialization();
             InitializeCosmosDB();
 
             Console.ReadLine();
@@ -48,17 +49,31 @@ namespace ConsoleInterface
             {
                 Console.WriteLine(String.Format("Readed from SQL Server: ProductId {0} - {1}", item.ProductId, item.Name));
 
-                await InsertProductInCosmosDb(item);
+                //680 is the one used for testing
+                if(item.ProductId!=680)
+                    await InsertProductInCosmosDb(item);
 
                 Console.WriteLine("Inserted Product in CosmosDB");
             }
+        }
+
+        private static async void TestCosmosDBInitialization()
+        {
+            /// Get the products
+            var p = GetProduct(680);
+
+            Console.WriteLine(String.Format("Readed from SQL Server: ProductId {0} - {1}", p.Result.ProductId, p.Result.Name));
+
+            await InsertProductInCosmosDb(p.Result);
+
+            Console.WriteLine("Inserted Product in CosmosDB");
         }
 
         #region ApiCalls
 
         private static async Task<List<Product>> GetProducts()
         {
-            var users = new List<Product>();
+            var retorno = new List<Product>();
 
             using (HttpClient client = new HttpClient())
             {
@@ -70,11 +85,32 @@ namespace ConsoleInterface
                 response.EnsureSuccessStatusCode();
                 if (response.IsSuccessStatusCode)
                 {
-                    users = await response.Content.ReadAsAsync<List<Product>>();
+                    retorno = await response.Content.ReadAsAsync<List<Product>>();
                 }
             }
 
-            return users;
+            return retorno;
+        }
+
+        private static async Task<Product> GetProduct(int productid)
+        {
+            var retorno = new Product();
+
+            using (HttpClient client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(_urlApiSqlServer);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+                HttpResponseMessage response = await client.GetAsync(String.Format("api/products/{0}",productid));
+                response.EnsureSuccessStatusCode();
+                if (response.IsSuccessStatusCode)
+                {
+                    retorno = await response.Content.ReadAsAsync<Product>();
+                }
+            }
+
+            return retorno;
         }
 
         private static async Task<Product> InsertProductInCosmosDb(Product product)
